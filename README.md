274429f3718c2cdc835aee18d99868a647ddabca
Kacppian <aspkaushik@gmail.com>
2017-08-06 11:47:17 +0530

    Cleaned folder for git

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-scaffold-bootstrap-webpack-babel-sass
-React scaffold that I use for my projects with Sass, webpack, React, and Babel. 
---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import '../styles/styles.sass';
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+
+
+ReactDOM.render(<App />, document.getElementById('react-container'));
